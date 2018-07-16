61352a75a199126b9ddc41bb33a517f9bf62f1ea
Ali <ali@manish-tn80812.in.corp.tavant.com>
2018-07-16 12:23:56 +0530

    Making final changes

--- src/service.ts
+++ src/service.ts
@@ -13,20 +13,21 @@ export class Service{
         constructor(private http: Http){
 	}
 
-    // private url:string = "http://192.168.253.108:5000/";For Ritz-Carlton
-    private url:string = "http://10.5.50.156:5000/";
+    private url:string = "http://192.168.253.108:5000/";//For Ritz-Carlton
+    // private url:string = "http://10.5.50.156:5000/";
 
    getResponse(body: Object) : Observable<any> {
 
         let bodyString = JSON.stringify(body); // Stringify payload
         let headers      = new Headers({ 'Content-Type': 'application/json' }); // ... Set content type to JSON
+        headers.append('Access-Control-Allow-Origin','*');
         let options       = new RequestOptions({ headers: headers }); // Create a request option
 
 
          return this.http.post(this.url, body, options) // ...using post request
                          .map((res:Response) => res.json()) // ...and calling .json() on the response to return data
                          .catch((error:any) => Observable.throw(error.json().error || 'Server error')); //...errors if any
-      
+
 
      }
 
